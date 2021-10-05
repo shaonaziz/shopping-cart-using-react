6a1b05e88cde8b055aeed3e6206e941b28b05430
Shaonaziz <80143099+shaonaziz@users.noreply.github.com>
2021-10-05 12:23:56 +0600

    deploy my shopping cart

---
+++ src/data.js
@@ -0,0 +1,34 @@
+const data = {
+  products: [
+    {
+      id: 1,
+      name: "Macbook",
+      price: 2300,
+      image:
+        "https://agbd.s3.amazonaws.com/media/images/Macbook-Aidr--M1-d-1.jpg",
+    },
+    {
+      id: 2,
+      name: "Echo Dot ",
+      price: 1800,
+      image:
+        "https://images-na.ssl-images-amazon.com/images/I/714Rq4k05UL._AC_UL200_SR200,200_.jpg",
+    },
+    {
+      id: 3,
+      name: "Fire HD 8 Kids tablet",
+      price: 4300,
+      image:
+        "https://images-na.ssl-images-amazon.com/images/I/61lgxCi9b7L._AC_UL200_SR200,200_.jpg",
+    },
+    {
+      id: 4,
+      name: "Apple AirTag 4 Pack",
+      price: 5300,
+      image:
+        "https://images-na.ssl-images-amazon.com/images/I/71gY9E%2BcTaS._AC_UL200_SR200,200_.jpg",
+    },
+    
+  ],
+};
+ export default data;
